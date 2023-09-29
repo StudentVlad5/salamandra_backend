e73fc1fec719cdc0b9e99b7176a93f0e762d438d
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-29 09:31:02 +0300

    come back changes

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
 const { errorHandler } = require('./helpers');
 const { routerAuth, routerMenu, routerAdmin, routerProduct } = require('./routes/api');
 
@@ -14,16 +9,10 @@ const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
   next();
 });
 app.use(express.urlencoded({ extended: true }));
