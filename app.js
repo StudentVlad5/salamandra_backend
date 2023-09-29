54d014127f4f6ed968821436b26f9b3fe371833b
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-29 07:13:02 +0300

    back

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-const { errorHandler } = require("./helpers");
-const {
-  routerAuth,
-  routerMenu,
-  routerAdmin,
-  routerProduct,
-} = require("./routes/api");
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+const { errorHandler } = require('./helpers');
+const { routerAuth, routerMenu, routerAdmin, routerProduct } = require('./routes/api');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("images"));
+app.use(express.static('images'));
 app.use("/uploads", express.static(`${__dirname}/images`));
 
-app.use("/api/auth", routerAuth);
-app.use("/api/menu", routerMenu);
-app.use("/api/product", routerProduct);
-app.use("/api/admin", routerAdmin);
+app.use('/api/auth', routerAuth);
+app.use('/api/menu', routerMenu);
+app.use('/api/product', routerProduct);
+app.use('/api/admin', routerAdmin);
 
 app.use((req, res) => {
-  console.log("!!!!! APP (req, res) !!!!!!");
+  console.log('!!!!! APP (req, res) !!!!!!');
   res.status(404);
-  res.json({ messages: "ERRR JSONS" });
+  res.json({ messages: 'ERRR JSONS' });
 });
 
 app.use(errorHandler);
